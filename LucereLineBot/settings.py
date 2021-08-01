66662371ac518ff95432b49b8421318079eafcf1
Rainburn <cyber221168@yahoo.com>
2021-08-01 14:15:28 +0700

    hello world test

--- LucereLineBot/settings.py
+++ LucereLineBot/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import os
+import environ
+
+env = environ.Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u*x%l08cm3k6cmr*0)&q-o4d#p%mfzw%g$od6x2)81ac6(q_f5'
+SECRET_KEY = env("SECRET_KEY")
+LINE_CHANNEL_SECRET = env("LINE_CHANNEL_SECRET")
+LINE_CHANNEL_ACCESS_TOKEN = env("LINE_CHANNEL_ACCESS_TOKEN")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -32,6 +39,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'lucere_bot',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- lucere_bot/views.py
+++ lucere_bot/views.py
@@ -1,6 +1,49 @@
+from django.conf import settings
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+
+line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(settings.LINE_CHANNEL_SECRET)
+
 
 # Create your views here.
 def index(request):
     return HttpResponse("Test")
+
+@csrf_exempt
+def callback(request):
+    if (request.method == 'POST'):
+        # get X-Line-Signature header value
+        signature = request.META['HTTP_X_LINE_SIGNATURE']
+        global domain
+        domain = request.META['HTTP_HOST']
+
+        # get request
+        body = request.body.decode('utf-8')
+
+        # handle webhook body
+        try:
+            handler.handle(body, signature)
+        except InvalidSignatureError:
+            return HttpResponseBadRequest()
+        
+        return HttpResponse()
+    
+    else :
+        return HttpResponseBadRequest()
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    msg_from_user = event.message.text
+    if (msg_from_user == "Hello"):
+        message = TextSendMessage("yoo bray")
+        line_bot_api.reply_message(event.reply_token, message)
+    else :
+        message = TextSendMessage(text=event.message.text)
+        line_bot_api.reply_message(event.reply_token, message)
