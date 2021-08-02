4c296a6432d636ed2b779b16e2d8dc0858e90dad
Rainburn <cyber221168@yahoo.com>
2021-08-02 12:13:30 +0700

    rough first app, tests required

--- lucere_bot/views.py
+++ lucere_bot/views.py
@@ -6,6 +6,7 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from datetime import datetime
+from .models import *
 
 
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
@@ -43,10 +44,192 @@ def callback(request):
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+
+    # command guide 
+    # create event : /create [event_name] [where/site/game] [time-userdefined]
+    # join event : /join [event-name]-[event-id]
+    # register : /register [nickname]
+    # rename : /rename [nickname]
+    # leave event : /leave [event_name]-[event-id]
+
+    # original message
     msg_from_user = event.message.text
-    if (msg_from_user == "Hello"):
-        message = TextSendMessage(text=str(event.source.user_id))
+
+    # check whether it's a command or chat
+    if (msg_from_user[0] != "/"): # not command
+        return
+
+    parameters = msg_from_user[1:len(msg_from_user)+1].split(" ")
+
+    command = parameters[0]
+
+    if (command == "register"):
+        user_id = event.source.user_id
+
+        if (len(parameters) == 1): # no nickname was given
+            try:
+                profile = line_bot_api.get_profile(userid)
+                line_username = profile.display_name
+                register(user_id, line_username)
+                return
+            
+            except LineBotApiError as e:
+                show_error_msg(event.reply_token, "Cannot register. Please add the bot as your friend or include your nickname after 'register' command")
+                return
+
+
+        nickname = " ".join(parameters[1:len(parameters)+1])
+        
+
+        if (is_user_registered(event.reply_token, user_id)):
+            return
+
+        register(user_id, nickname)
+
+    elif (command == "rename"):
+        user_id = event.source.user_id
+
+        if (len(parameters) == 1): # no new nickname was given
+            show_error_msg(event.reply_token, "Failed to rename. New nickname cannot be empty")
+            return
+
+        if not (is_user_registered(event.reply_token, user_id)):
+            show_error_msg(event.reply_token, "Only registered users can use 'rename' command")
+            return
+
+        nickname = " ".join(parameters[1:len(parameters)+1])
+        rename(user_id, nickname)
+
+    elif (command == "create"):
+        try:
+            event_name = parameters[1]
+            event_site = parameters[2]
+            event_time = parameters[3]
+            event_id = add_event(event_name, event_site, event_time)
+
+            # Event creator auto join the event
+            user_id = event.source.user_id
+            join_event(event_id, user_id)
+
+            # TODO: show event details
+
+        except:
+            show_error_msg(event.reply_token)
+
+    elif (command == "join"):
+        try :
+            event_details  = parameters[1].split("-")
+            event_id = event_details[1]
+            user_id = event.source.user_id
+            join_event(event_id, user_id)
+        
+            # TODO: show event details
+        
+        except:
+            show_error_msg(event.reply_token, "Failed to join event!")
+
+    elif (command == "leave"):
+        try:
+            event_details  = parameters[1].split("-")
+            event_id = event_details[1]
+            user_id = event.source.user_id
+            leave_event(event_id, user_id)
+        except:
+            show_error_msg(event.reply_token, "Invalid event ID !")
+
+    else : # Command not found
+        warn_text = "Command '" + command + "' not found !"
+        message = TextSendMessage(text=warn_text)
         line_bot_api.reply_message(event.reply_token, message)
-    else :
-        message = TextSendMessage(text=event.message.text)
-        line_bot_api.reply_message(event.reply_token, message)
+    
+
+def show_msg(reply_token, msg):
+    message = TextSendMessage(text=msg)
+    line_bot_api.reply_message(reply_token, message)
+
+def show_error_msg(reply_token, msg=""):
+    error_msg = "Invalid command parameters !" if msg == "" else msg
+    message = TextSendMessage(text=error_msg)
+    line_bot_api.reply_message(reply_token, message)
+
+def register(user_id, nickname):
+    new_user = User(userid=user_id,nickname=nickname)
+    new_user.save()
+    return new_user.id
+
+def rename(user_id, nickname):
+    user = User.objects.get(userid=user_id)
+    user.nickname = nickname
+    user.save()
+
+def add_event(name, site, when):
+    new_event = Event(name=name, site=site, when=when)
+    new_event.save()
+
+def join_event(eventid, userid):
+
+    # check whether user has already join the event
+    result = list(EventParticipant.objects.filter(eventid=eventid, userid=userid))
+    if result:
+        return
+
+    new_member = EventParticipant(eventid=eventid, userid=userid)
+    new_member.save()
+
+def leave_event(eventid, userid):
+    instance = EventParticipant.objects.get(eventid=eventid, userid=userid)
+    instance.delete()
+    return
+
+def is_user_registered(replytoken, userid):
+    result = list(User.objects.filter(userid=userid))
+    if not result: # User not registered
+        
+        # get user line profile
+        try:
+            profile = line_bot_api.get_profile(userid)
+            line_username = profile.display_name
+            register(userid, line_username)
+
+        except LineBotApiError as e:
+            error_msg = TextSendMessage(text="Something's wrong. Adding Lucere BOT may fix the issue.")
+            line_bot_api.reply_message(replytoken, error_msg)
+            return
+
+    return
+
+def show_event_details(reply_token, eventid):
+    # Structure
+    # Event ID : [event_name]-[event_id]
+    # [event_name]
+    # [event_site]
+    # [event_when]
+    # [list of members]
+
+    result = ""
+    
+    try :
+        event = Event.objects.get(id=eventid)
+        event_name_id = event.name + "-" + event.id + '\n'
+        event_name = event.name + '\n'
+        event_site = event.site + '\n'
+        event_when = event.when + '\n'
+
+        result = event_name_id + event_name + event_site + event_when
+
+        members = list(EventParticipant.objects.filter(eventid=eventid))
+        for i in range(len(members)):
+            user_id = members[i].userid
+
+            user = User.objects.get(userid=user_id)
+            nickname = user.nickname
+            
+            currMember = f'{i+1}. {nickname}\n'
+            result = result + currMember
+
+
+        show_msg(reply_token, result)
+
+    except Event.DoesNotExist:
+        show_error_msg(reply_token, eventid)
+        return
