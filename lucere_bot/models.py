ca8f636bbfc5da9599eaf34a3dc12012371835be
Rainburn <cyber221168@yahoo.com>
2021-08-02 16:09:22 +0700

    test 7

--- lucere_bot/models.py
+++ lucere_bot/models.py
@@ -9,13 +9,22 @@ class Event(models.Model):
     when = models.CharField(max_length = 150)
     dateissued = models.DateTimeField(default=datetime.now, blank=True)
 
+    def __str__(self):
+        return self.name + '-' + str(self.id)
+
 
 class User(models.Model):
     userid = models.CharField(max_length = 200, unique=True)
     nickname = models.CharField(max_length = 50)
 
+    def __str__(self):
+        return self.nickname
+
 
 class EventParticipant(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.event) + ", " + str(self.user)
 
